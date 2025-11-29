715bdb628c3a8c06b8c11b6232f6e4dadc256477
steven <large_world@163.com>
2025-11-29 13:41:56 +0800

    fix port

--- config/vite.config.dev.ts
+++ config/vite.config.dev.ts
@@ -1,24 +1,29 @@
-import { mergeConfig } from 'vite'
+import { loadEnv, mergeConfig } from 'vite'
 import eslint from 'vite-plugin-eslint'
 import baseConfig from './vite.config.base'
 
-export default mergeConfig(
-  {
-    mode: 'development',
-    server: {
-      open: true,
-      host: '0.0.0.0',
-      fs: {
-        strict: true,
+export default ({ mode }) => {
+  // 加载 env 文件
+  const env = loadEnv(mode, process.cwd())
+  console.log('VITE_PORT:', env.VITE_PORT)
+
+  return mergeConfig(
+    {
+      mode,
+      server: {
+        open: true,
+        host: '0.0.0.0',
+        port: Number(env.VITE_PORT) || 5173,
+        strictPort: true,
       },
+      plugins: [
+        eslint({
+          cache: false,
+          include: ['src/**/*.ts', 'src/**/*.tsx', 'src/**/*.vue'],
+          exclude: ['node_modules'],
+        }),
+      ],
     },
-    plugins: [
-      eslint({
-        cache: false,
-        include: ['src/**/*.ts', 'src/**/*.tsx', 'src/**/*.vue'],
-        exclude: ['node_modules'],
-      }),
-    ],
-  },
-  baseConfig
-)
+    baseConfig
+  )
+}
